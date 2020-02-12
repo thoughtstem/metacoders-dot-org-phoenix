ce00191425e0c7f2839f06009d82fd1686e04f6c
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2020-02-12 14:40:04 -0800

    added weekly classes

--- metacoders-dot-org-phoenix-site/main.rkt
+++ metacoders-dot-org-phoenix-site/main.rkt
@@ -8,22 +8,40 @@
 ; If you are creating a brand new course,
 ; run (generate-random-sku) to generate a unique sku id
 
-; ========== EXAMPLE COURSE ==========
-#;(define/provide-course weekly-class-k-2nd-winter
+(define/provide-course weekly-class-k-2nd-spring 
   (course
-      ;#:topic         "Awesome Animals!"
-      ;#:sku           "sku_GKwkIwx91IPMXI"
-      ;#:video-path    healer-animal-mp4-path
-      ;#:description   "MetaCoders’ novel approach to computer science education is backed by the latest scientific research that shows learning how to code is more like learning a foreign language than learning math. Therefore, we leverage the entire field of language acquisition so that our students are ready for a future where they’ll need to speak the languages of computers fluently. We also keep the emphasis on FUN, so throughout the course students will earn badges and prizes as they expand their knowledge of computer science. During this session of Coding Club, students will be designing video games featuring animals - they'll learn how to customize the colors of & heal their animal game characters, and also avoid enemies!"
-      ;#:grade-range   "K - 2nd"
-      ;#:location      "University of Dallas"
-      ;#:address       "1845 E Northgate Dr, Irving, TX 75062"
-      ;#:address-link  "https://goo.gl/maps/TBgDvXgp5esLrKLw7"
-      ;#:price         210
-      ;#:start-time    "10:00am"
-      ;#:end-time      "11:30am"
-      ;#:meeting-dates (list "2/8/2020" "2/15/2020" "2/22/2020" "2/29/2020" "3/7/2020" "3/14/2020" "3/21/2020")
-      ;#:status        'open
+      #:topic         "Awesome Animals!"
+      #:sku           "sku_tDc2Uev3SyuhiNcaah4Xw"
+      #:video-path     k2-animal-mp4-path
+      #:description   "MetaCoders’ novel approach to computer science education is backed by the latest scientific research that shows learning how to code is more like learning a foreign language than learning math. Therefore, we leverage the entire field of language acquisition so that our students are ready for a future where they’ll need to speak the languages of computers fluently. We also keep the emphasis on FUN, so throughout the course students will earn badges and prizes as they expand their knowledge of computer science. During this session of Coding Club, students will be designing video games featuring animals - they'll learn how to customize the colors of & heal their animal game characters, and also avoid enemies!"
+      #:grade-range   "K - 2nd"
+      #:location      "Grand Canyon University"
+      #:address       "3300 W Camelback Rd, Phoenix, AZ 85017"
+      #:address-link  "https://goo.gl/maps/xgxWwQJCKsVx4yt37"
+      #:price         130
+      ;#:discount      13
+      #:start-time    "10:30am"
+      #:end-time      "12:00pm"
+      #:meeting-dates (list "04/04/2020" "04/18/2020" "05/09/2020" "05/16/2020" "05/23/2020")
+      #:status        'open
+   ))
+   
+(define/provide-course weekly-class-3rd-6th-spring
+  (course
+      #:topic         "Survival Video Game Design"
+      #:sku           "sku_1w8s8UG0Tr2Bm8poPT457A"
+      #:video-path     survival-mp4-path
+      #:description   "In our Winter Saturday class, students will design & code their very own 2D survival games. Students will learn how to build critical features that make survival games like Minecraft and Fortnite fun! Students will use a powerful, typed language to code features like health bars, day-night cycles, and crafting/recipe mechanics. At the end of this course, students will be able to share their survival games with friends and family."
+      #:grade-range   "3rd-6th"
+      #:location      "Grand Canyon University"
+      #:address       "3300 W Camelback Rd, Phoenix, AZ 85017"
+      #:address-link  "https://goo.gl/maps/xgxWwQJCKsVx4yt37"
+      #:price         130
+     ;#:discount      13
+      #:start-time    "12:30pm"
+      #:end-time      "2:00pm"
+      #:meeting-dates (list "04/04/2020" "04/18/2020" "05/09/2020" "05/16/2020" "05/23/2020")
+      #:status 'open
    ))
 
 ; ========== EXAMPLE CAMP ==========
@@ -53,7 +71,8 @@
         (city-page
          #:city-name "Phoenix, AZ"
          #:banner-url (prefix/pathify phoenix-banner-path)
-         ;#:school-year-courses (list weekly-class-k-2nd-winter)
+         #:school-year-courses (list weekly-class-k-2nd-spring
+                                     weekly-class-3rd-5th-spring)
          ;#:summer-camps (list summer-camp-clicker-k-2nd-7/15)
          ;#:camp-pricing (summer-camp-pricing-at #:location "Gallaudet University"
          ;                                       #:am-camp-time "9am - 1pm"
